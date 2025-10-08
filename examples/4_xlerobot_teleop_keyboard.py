cbb4acb966123e80138a35411bfcdaa6ff639f8c
Carl Kesselman <carl@isi.edu>
2025-10-07 22:08:25 -0700

    Modified teleop to use draccus initialization.

--- examples/4_xlerobot_teleop_keyboard.py
+++ examples/4_xlerobot_teleop_keyboard.py
@@ -11,8 +11,10 @@ PYTHONPATH=src python -m examples.xlerobot.teleoperate_Keyboard
 import time
 import numpy as np
 import math
+import draccus
 
 from lerobot.robots.xlerobot import XLerobotClient, XLerobotClientConfig, XLerobotConfig, XLerobot
+from lerobot.robots import RobotConfig
 from lerobot.utils.robot_utils import busy_wait
 from lerobot.utils.visualization_utils import init_rerun, log_rerun_data
 from lerobot.model.SO101Robot import SO101Kinematics
@@ -384,21 +386,25 @@ def main():
     FPS = 50
     # ip = "192.168.1.123"  # This is for zmq connection
     ip = "localhost"  # This is for local/wired connection
-    robot_name = "rosie"
+    robot_name = "my_xlerobot_pc"
 
+    robot_config = draccus.parse(config_class=RobotConfig)
+    print(f"Configuring robot of type: {robot_config.type}")
     # For zmq connection
     # robot_config = XLerobotClientConfig(remote_ip=ip, id=robot_name)
     # robot = XLerobotClient(robot_config)    
 
     # For local/wired connection
-    robot_config = XLerobotConfig(id=robot_name)
+    #robot_config = XLerobotConfig()
     robot = XLerobot(robot_config)
-
+    
     try:
         robot.connect()
         print(f"[MAIN] Successfully connected to robot")
     except Exception as e:
         print(f"[MAIN] Failed to connect to robot: {e}")
+        print(robot_config)
+        print(robot)
         return
         
     init_rerun(session_name="xlerobot_teleop_v2")
